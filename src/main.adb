66a47748b29265066bad836bf9ed26bd0bac06e8
Ruifeng Liu <ruifengl3@student.unimelb.edu.au>
2021-05-24 15:53:50 +1000

    added assertions for task 4

--- src/main.adb
+++ src/main.adb
@@ -1,3 +1,21 @@
+--Task 4
+--1) Proof of Unlock operation can only be performed when the calculator is in the locked state:
+-- A pragama assertion is added in line (?) before the Unlock operation happens. It confirm that
+-- LockedState is True before changing it to False.
+
+--2) Proof of Lock operation successfully updates the PIN with a new PIN supplied:
+-- A pragma assertion is added in line (?) after the value of PIN.From_String(S) is passed to
+-- PINOriginal. This assertion checks if the master PIN is successfully changed to the new one.
+-- And this assertion is measured for each iteration of the while loop.
+
+--3) Proof of "+", "-", "*", "/", load, store, remove, and lock operations can only ever be 
+--   performed when the calculator is unlocked:
+-- Our implementation of the calculator is designed to distinguish between Locked and Unlocked states
+-- by a if statement in line (?) which regulates above operations to be executed only when LockedState
+-- is False. To let SPARK prove this, we have added pragma assertion before each of above operations to
+-- check if LockedState = False holds.
+
+
 pragma SPARK_Mode (On);
 
 with StringToInteger;
@@ -5,7 +23,7 @@ with VariableStore;
 with MyCommandLine;
 with MyString;
 with MyStringTokeniser;
-with PIN;
+with PIN; use PIN; -- added use clause for use in pragma assertion
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Containers;
@@ -83,6 +101,9 @@ begin
                         PINAttempt  : PIN.PIN := PIN.From_String(S); 
                      begin
                         If PIN."="(PINAttempt, PINOriginal) then
+                           
+                           pragma Assert(LockedState = True); -- Unlock can only perform when its Locked
+                           
                            LockedState := False;
                         else 
                            Put("Wrong PIN. Please try again.");New_Line;
@@ -101,8 +122,14 @@ begin
                    S : String := Lines.To_String(Input);
                begin
                   if S'Length = 4 and (for all I in S'Range => S(I) >= '0' and S(I) <= '9') then
+                     
+                     pragma Assert(LockedState = False); -- Added to secure operations
+                     
                      PINOriginal := PIN.From_String(S);
                      LockedState := True;
+                     
+                     pragma Assert(LockedState = True and PINOriginal = PIN.From_String(S)); -- check if PIN is updated after locking
+                     
                   else 
                      Put("You must enter a valid PIN.");New_Line;
                   end if;
@@ -113,6 +140,9 @@ begin
                      inputInteger : Integer := StringToInteger.From_String(Lines.To_String(Input));
                   begin
                      if inputInteger >= 0 then
+                        
+                        pragma Assert(LockedState = False); -- Added to secure operations
+                        
                         SS.Push(Stack, inputInteger);
                      else
                         Put("Negative number is not allowed.");
@@ -125,6 +155,9 @@ begin
                end if;
             elsif Lines.To_String(Command) = "pop" then
                if SS.Size(Stack) > 0 then
+                  
+                  pragma Assert(LockedState = False); -- Added to secure operations
+                  
                   SS.Pop(Stack);
                else 
                   Put("Cannot pop on an empty stack.");New_Line;
@@ -142,14 +175,18 @@ begin
                      SS.PopWithResult(Stack, integerA);
                      SS.PopWithResult(Stack, integerB);
                      if Lines.To_String(Command) = "+" and (integerB >= 0 and then integerA < Integer'Last - integerB) then
+                        pragma Assert(LockedState = False); -- Added to secure operations
                         SS.Push(Stack, integerA + integerB);
                      elsif Lines.To_String(Command) = "-" and (integerB >= 0 and then integerA > Integer'First + integerB) then
+                        pragma Assert(LockedState = False); -- Added to secure operations
                         SS.Push(Stack, integerA - integerB);
                      elsif (Lines.To_String(Command) = "*" and (integerA >= 0 and integerB >= 0)) and then
                        (Long_Long_Integer(integerA) * Long_Long_Integer(integerB)) <= Long_Long_Integer(Integer'Last) then
+                        pragma Assert(LockedState = False); -- Added to secure operations
                         SS.Push(Stack, integerA * integerB);
                      elsif Lines.To_String(Command) = "/" then
                         if integerB /= 0 and integerA > 0 and integerB > 0 then
+                           pragma Assert(LockedState = False); -- Added to secure operations
                            SS.Push(Stack, integerA / integerB);
                         else
                            Put("Division by zero is disallowed.");New_Line;
@@ -173,6 +210,7 @@ begin
                         begin
                            SS.PopWithResult(Stack, poppedInteger);
                            if VariableStore.Length(DB) < VariableStore.Max_Entries then
+                              pragma Assert(LockedState = False); -- Added to secure operations
                               VariableStore.Put(DB,VariableName,poppedInteger);
                            end if;
                         end;
@@ -183,6 +221,9 @@ begin
                   return;
                end if;
             elsif Lines.To_String(Command) = "list" then
+               
+               pragma Assert(LockedState = False); -- Added to secure operations
+               
                VariableStore.Print(DB);
             elsif Lines.To_String(Command) = "load" then
                declare 
@@ -193,7 +234,10 @@ begin
                         VariableName : VariableStore.Variable := VariableStore.From_String(S);
                      begin
                         if VariableStore.Has_Variable(DB, VariableName) then
-                            if SS.Size(Stack) < StackSize - 1 then
+                           if SS.Size(Stack) < StackSize - 1 then
+                              
+                              pragma Assert(LockedState = False); -- Added to secure operations
+                              
                               SS.Push(Stack, VariableStore.Get(DB,VariableName));
                            end if;
                         else
@@ -210,7 +254,8 @@ begin
                      if S'Length > 0 and S'Length < VariableStore.Max_Variable_Length then
                declare
                   VariableName : VariableStore.Variable := VariableStore.From_String(S);
-               begin
+                     begin
+                        pragma Assert(LockedState = False); -- Added to secure operations
                   If VariableStore.Has_Variable(DB,VariableName) then
                      VariableStore.Remove(DB,VariableName);
                   else 
